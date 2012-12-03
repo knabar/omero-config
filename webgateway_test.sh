4393dedffc92ae43d9389587f3255f5657f6675a
Andreas Knab <knabar@gmail.com>
2012-12-02 21:24:27 -0500

    Add utility script to run certain webgateway unit tests

---
+++ webgateway_test.sh
@@ -0,0 +1,11 @@
+cd ~/omero/openmicroscopy/components/tools/OmeroWeb
+TESTS=
+TESTS="$TESTS webgateway.RepositoryApiTest"
+TESTS="$TESTS webgateway.ManagedRepositoryApiTest"
+TESTS="$TESTS webgateway.RepositoryApiAsAuthorTest"
+TESTS="$TESTS webgateway.ManagedRepositoryApiAsAuthorTest"
+TESTS="$TESTS webgateway.RepositoryApiPermissionsTest"
+TESTS="$TESTS webgateway.ManagedRepositoryApiPermissionsTest"
+TESTS="$TESTS webgateway.ManagedRepositoryApiListTest"
+
+python omeroweb/manage.py test $TESTS
